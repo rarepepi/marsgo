8c5b57c10fb72bf384ef09d5e7b6f16226870cf4
rarepepi <pepimartinez@protonmail.com>
2023-03-17 15:00:43 -0400

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         loop
         ref={audioRef}
       />{" "}
-      <main className="flex flex-col items-center  text-center p-12  h-screen text-white bg-mars bg-cover bg-center bg-no-repeat ">
+      <main className="flex flex-col items-center  text-center p-12 text-white bg-mars bg-cover bg-center bg-no-repeat ">
         <div className="max-w-4xl backdrop-blur-sm rounded-2xl">
           <h1 className="mt-6 text-6xl font-bold">
             Your First-Class Ticket to the Red Frontier!
@@ -35,16 +35,16 @@ export default function Home() {
             passionate, in fact, we&apos;re almost as real as those other
             startups that will never see the light of day.
           </p>
-          <div className="mt-8 text-2xl">
+          <div className="my-8 text-2xl">
             <h3>
               Join us in our quest to conquer the universe (at least in our
               dreams). Here&apos;s what MarsGo.xyz has to offer:
             </h3>
 
-            <ol className="text-lg mt-4">
+            <ol className="text-lg mt-4 space-y-4">
               <li>
-                <span className="text-2xl">
-                  1. Exclusive Martian Real Estate 🏠
+                <span className="text-2xl font-bold">
+                  1. Exclusive Martian Real Estate 🏠 <br />
                 </span>
                 <br /> Tired of Earth&apos;s limited space and overpriced
                 properties? Reserve your own Martian land now! With our
@@ -53,7 +53,9 @@ export default function Home() {
                 Or the land. Ever.
               </li>
               <li>
-                <span className="text-2xl">2. MarsGo Social Network 🌐</span>
+                <span className="text-2xl font-bold">
+                  2. MarsGo Social Network 🌐 <br />
+                </span>
                 <br /> Introducing MarsGo&apos;s revolutionary social network,
                 MarsBook! Connect with fellow Earthlings dreaming of Mars. Share
                 memes, have a laugh, and discuss the best way to grow potatoes
@@ -61,7 +63,7 @@ export default function Home() {
                 progress.
               </li>
               <li>
-                <span className="text-2xl">3. MarsGo Fitness 🏋️‍♀️</span>
+                <span className="text-2xl font-bold">3. MarsGo Fitness 🏋️‍♀️</span>
                 <br /> Stay fit for your imaginary journey to Mars with our
                 MarsGo Fitness app. Get tips from our not-so-certified trainers
                 on how to survive in the Martian environment without ever
@@ -69,14 +71,18 @@ export default function Home() {
                 out, but we&apos;re working on it (kind of).
               </li>
               <li>
-                <span className="text-2xl">4. MarsGo Merchandise 🛍️</span>
+                <span className="text-2xl font-bold">
+                  4. MarsGo Merchandise 🛍️ <br />
+                </span>
                 <br /> Show your support for MarsGo and look out-of-this-world
                 in our exclusive MarsGo merchandise. We have T-shirts, caps,
                 hoodies, and more! Perfect for confusing your friends and
                 sparking conversations about your favorite parody startup.
               </li>
               <li>
-                <span className="text-2xl">5. MarsGo Jobs 🚀</span>
+                <span className="text-2xl font-bold">
+                  5. MarsGo Jobs 🚀 <br />
+                </span>
                 <br /> Looking for a career that&apos;s going nowhere? Join the
                 MarsGo team! We&apos;re always looking for talented individuals
                 who are passionate about not actually doing anything. It&apos;s
@@ -85,12 +91,12 @@ export default function Home() {
               </li>
             </ol>
           </div>
-          <p className="mt-8 text-2xl">
+          <p className="mt-8 text-2xl backdrop-blur-sm">
             Sign up today and join our not-so-serious mission to Mars. Together,
             we&apos;ll reach for the stars (and maybe even get a chuckle or two
             along the way).
           </p>
-          <h3 className="my-4">
+          <h3 className="my-4 backdrop-blur-sm">
             MarsGo.xyz - Because the universe needs a bit of humor.
           </h3>
         </div>
